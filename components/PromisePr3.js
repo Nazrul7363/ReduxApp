ad18c28ad123cd5cabc2e05c7beaf36c841dbb5c
Nazrul7363 <68236364+Nazrul7363@users.noreply.github.com>
2024-01-10 10:25:23 +0530

    Update PromisePr3.js

--- components/PromisePr3.js
+++ components/PromisePr3.js
@@ -1,18 +1,1 @@
-// let arr=[2,3,5,7];
-//  function f(arr,callback){
-//     let doubleArr=arr.map((num)=>{
-//        return  callback(num);
-//     })
-//     return doubleArr;
-//  };
-
-//  function callback(num){
-//     return num*2;
-//  };
-
-//  let returnArray=f(arr,callback);
-//  console.log(returnArray);
-
-
-function manipulateString(string,){}
 
